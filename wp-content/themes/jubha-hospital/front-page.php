49d104a89ff0a7e5cad6db50cfaf1fe55b9a25b4
vannararin23-sketch <vannararin23@gmail.com>
2026-01-09 13:48:38 +0700

    Desgin About page

---
+++ wp-content/themes/jubha-hospital/front-page.php
@@ -0,0 +1,4 @@
+<?php get_header(); ?>
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/front-page.css">
+
+<div class="more-space"></div>
---
+++ wp-content/themes/jubha-hospital/header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    
+<head>
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/header.css">
+
+    <?php wp_head(); ?>
+    
+
+</head>
+<body <?php body_class(); ?>>
+
+
+
+<header class="header">
+    <img src="" alt="">
+    <div class="header-img"></div>
+    <!-- <h1 class="site-title"><?php bloginfo('name'); ?></h1> -->
+    <div class="icon-space"> 
+        
+    </div>
+    
+    <div class="menu" id="mobile-menu">
+        <?php 
+        wp_nav_menu([
+            'theme_location' => 'primary',
+            'menu_class' => 'menu-list',
+            'container' => false
+        ]); 
+        ?>
+        <!-- <i class="fa-solid fa-list phone-only" id="menu-toggle"></i> -->
+         <div class="online-call">
+            <a href=""><i class="fa-solid fa-headset phone-only-call"></i></a>
+            <a href="<?php echo get_permalink( get_page_by_path('doctor-schedule') ); ?>">
+                <h1><i class="fa-solid fa-calendar-days phone-only"></i> Online reservation</h1>
+            </a>
+        </div>
+
+    </div>
+</header>
+
+
+
+
+
+
+
+
+
+
+
+</script>
+
+
+
+<main>
